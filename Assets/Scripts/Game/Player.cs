6da772f7085826f57b2ee8334476c4dce282e81e
Dmytrii Petrinko <petrinko6@gmail.com>
2019-11-02 02:48:50 +0200

    Added player with base movement and dashing. Renamed scene to Game

---
+++ Assets/Scripts/Game/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+	[RequireComponent(typeof(PlayerInput))]
+	[RequireComponent(typeof(PlayerMovement))]
+	public sealed class Player : MonoBehaviour
+	{
+		private PlayerInput playerInput;
+		private PlayerMovement playerMovement;
+
+		private void Awake()
+		{
+			playerInput = GetComponent<PlayerInput>();
+			playerMovement = GetComponent<PlayerMovement>();
+			playerInput.Dash += () => playerMovement.PerformDash(playerInput.MovementDirection);
+		}
+
+		private void FixedUpdate() => playerMovement.PerformMovement(playerInput.MovementDirection);
+	}
+}
